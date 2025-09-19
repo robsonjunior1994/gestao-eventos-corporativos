a880dc12476c83f2260508fa4005ff05b80a1886
Robson Junior <robsonjunior1994@gmail.com>
2025-09-19 10:19:29 -0300

    Ajustando tudo para uma janela só

---
+++ GestaoEventosCorporativos/GestaoEventosCorporativos.Wpf/Views/LoginView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GestaoEventosCorporativos.Wpf.Views
+{
+    public partial class LoginView : UserControl
+    {
+        private readonly MainWindow _main;
+
+        public LoginView(MainWindow main)
+        {
+            InitializeComponent();
+            _main = main;
+        }
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            _main.Navigate(new RegisterView(_main));
+        }
+
+        private void Entrar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Login realizado com sucesso!");
+        }
+    }
+}
---
+++ GestaoEventosCorporativos/GestaoEventosCorporativos.Wpf/Views/RegisterView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GestaoEventosCorporativos.Wpf.Views
+{
+    public partial class RegisterView : UserControl
+    {
+        private readonly MainWindow _main;
+
+        public RegisterView(MainWindow main)
+        {
+            InitializeComponent();
+            _main = main;
+        }
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Usuário cadastrado com sucesso!");
+
+            // Depois do cadastro, volta para tela de login
+            _main.Navigate(new LoginView(_main));
+        }
+    }
+}
